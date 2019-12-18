d7d340a11bcebec62beccbac4e5112c1c319cb7a
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-12-17 23:54:28 -0300

    modificando o background e as cores das labels

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,14 +2,23 @@ import { createAppContainer } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Data, Home } from '../container/container';
 
-const DrawerNav = createDrawerNavigator({
-  Home: {
-    screen: Home
-  },
+const DrawerNav = createDrawerNavigator(
+  {
+    Home: {
+      screen: Home
+    },
 
-  Data: {
-    screen: Data
+    Data: {
+      screen: Data
+    }
+  },
+  {
+    drawerBackgroundColor: '#303030',
+    contentOptions: {
+      inactiveTintColor: '#000',
+      activeTintColor: '#20832c'
+    }
   }
-});
+);
 
 export default createAppContainer(DrawerNav);
